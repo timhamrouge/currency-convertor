0541632d5b6d4b1e4fddda8362d58d8c66854978
timhamrouge <t.hamrouge@gmail.com>
2020-06-15 09:22:19 +0100

    formatting and adding error template

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use('/scripts', express.static(`${__dirname}/node_modules/`));
 
 // Listen for HTTP requests on port 3000
 app.listen(port, () => {
-  console.log('listening on %d', port);
+    console.log('listening on %d', port);
 });
